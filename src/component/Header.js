587aa9151f06d9b9b0cc8339023b7f5da2e37402
Pradyumna <pradyumn.bisoyi.java@gail.com>
2022-08-03 10:44:35 +0530

    first commit | component

---
+++ src/component/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const   Header = () => {
+    return (
+        <div>
+            <center><h1>Chatashali</h1>
+            <p>A Learning platform</p>
+            </center>
+            <hr/>
+        </div>
+    )
+}
+
+export default Header;
