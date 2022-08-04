a87f2052ad75a5baf012768fc6a8f965723a4f81
Pradyumna <pradyumn.bisoyi.java@gail.com>
2022-08-04 08:05:44 +0530

    inline css and object type css

--- src/component/Header.js
+++ src/component/Header.js
@@ -2,10 +2,17 @@ import React from 'react';
 
 class   Header extends React.Component {
     render () {
+        const mystyle = {
+            header: {
+                backgroundColor:'teal',
+                color:'white'
+            }
+        }
         return (
             <>
-                <center><h1>Chatashali</h1>
-                <p>A Learning platform</p>
+                <center style={mystyle.header}><h1>Chatashali</h1>
+                <input/>
+                <div style={{color:'red', fontSize:'20px'}}>User Text here</div>
                 </center>
                 <hr/>
             </>
